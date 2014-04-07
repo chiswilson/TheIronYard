c697c367885087d4ae1c065cf88c63a6b08ec7cb
Chisolm Wilson <cw@commonesystems.com>
2014-04-06 23:27:10 -0400

    just trying to commit, after some changes to desserts file

--- class_1/desserts.rb
+++ class_1/desserts.rb
@@ -26,24 +26,152 @@ class Dessert
 end
 
 class Candy < Dessert
-	attr_accessor :price, :flavor
-
-	# def initialize(name, price, flavor)
-	# 	@name = name
-	# 	@price = price
-	# 	@flavor = flavor
-	# end
+	attr_accessor :price, :units
+	def initialize(name, price)
+		@name = name
+		@price = price
+		@units = "/lb"
+	end
 end
 
 class Cookie < Dessert
-	attr_accessor :price, :flavor
+	attr_accessor :price, :units
+	def initialize(name, price)
+		@name = name
+		@price = price
+		@units = "/dozen"
+	end
 end
 
 class Icecream < Dessert
-	attr_accessor :price, :flavor
+	attr_accessor :price, :units
+	def initialize(name, price)
+		@name = name
+		@price = price
+		@units = "/scoop"
+	end
 end
 
 
-candy1 = Candy
+class Checkout
+	# try this instead of attr_accessor
+	attr_reader :subtotal
+	
+	def initialize
+	    @subtotal = 0.0
+		@items = Array.new
+	end
+
+	def show_items
+		@subtotal = 0.0
+		puts ""
+		puts "Your Shopping Cart:"
+		@items.each do |item| # item here is a hash   item[:item] is the actual item in the shopping cart
+			if item[:item].class == Candy
+			  unitstr = "lbs"
+		    elsif item[:item].class == Cookie
+			  unitstr = "dozen"
+		    else # ice cream
+			  unitstr = "scoop(s)"
+		    end
+		    itemtotal = item[:item].price * item[:qnty].to_f 
+
+			puts "  " + item[:item].name.ljust(25, " ") \
+			  + (" qty:" + item[:qnty].to_s + " " + unitstr).ljust(18, " ")  \
+			  + ("$" + sprintf('%.2f',itemtotal)).rjust(7, " ")
+
+			@subtotal += itemtotal  
+		end # @items.each
+		puts "  ---------------------"
+		puts "  Subtotal: $" + sprintf('%.2f', subtotal)
+		
+	end
+
+	def add_item(item, qnty)
+		@items.push({item: item, qnty: qnty})
+	end
+
+	def remove_item
+
+	end
+
+end
+
+# ***************
+# End of Classess
+# ***************
+
+def show_menu
+	system "clear"
+	puts "*** Welcome to Iron Yard Candy shop ***"
+	puts "*** TODAYS MENU:"	
+	# @menu_items.each do |item|
+    for i in 0..@menu_items.length-1 do
+    	item = @menu_items[i]
+		
+		puts ((i+1).to_s  + ")").ljust(9, " ") \
+		  + item.name.ljust(25, " ") \
+		  + "$" + sprintf('%.2f', item.price) + " " + item.units
+	end
+end
+
+
+
+# create my checkout
+checkout = Checkout.new
+
+# Array of available menu items
+@menu_items = Array.new
+@menu_items.push(Candy.new("Chocolate morsels", 2.99))
+@menu_items.push(Candy.new("Jelly beans", 2.50))
+@menu_items.push(Candy.new("Choc squares", 3.00))
+@menu_items.push(Candy.new("Reeses PB cups", 4.50))
+
+@menu_items.push(Cookie.new("Choc chip cookies", 4.50))
+@menu_items.push(Cookie.new("Mint cookies", 5.00))
+@menu_items.push(Cookie.new("Oatmeal raisin cookies", 4.50))
+
+@menu_items.push(Icecream.new("Chocolate chip icecream", 2.00))
+@menu_items.push(Icecream.new("Peppermint icecream", 2.50))
+@menu_items.push(Icecream.new("Coffee icecream", 2.75))
+
+
+
+# testing
+# checkout.add_item(@menu_items[0], 4)
+# checkout.add_item(@menu_items[1], 3)
+# checkout.add_item(@menu_items[5], 1)
+
+while true do
+	show_menu
+	checkout.show_items
+	puts ""
+	puts "Please enter a command:"
+	puts "(A = add item, D = delete item, C = check out, Q = quit)"
+	cmd = gets.chomp.upcase
+	break if cmd == 'Q'
+
+	case cmd
+	when "A"
+		puts "Please enter the item number from the menu:"
+		itemno = gets.chomp.to_i
+		if (1..@menu_items.length).include?(itemno)
+			puts "You selected: " + @menu_items[itemno-1].name
+			puts "enter quantity: (#{@menu_items[itemno-1].units})"
+			qnty = gets.chomp.to_i
+			checkout.add_item(@menu_items[itemno-1], qnty)
+
+		end
+	when "C"
+		puts "Subtotal: $" + sprintf('%.2f', checkout.subtotal)
+		puts "Tax:      $" + sprintf('%.2f', 0.08 * checkout.subtotal)     
+		puts "TOTAL:    $" + sprintf('%.2f', 1.08 * checkout.subtotal)  
+		puts ""
+		puts "enter any key to quit"
+		gets
+		break
+
+	end
+end
 
 
